a2abac9d862a35546eed9fe0bbcf1fab2d9717b4
Lcnvdl <programando.soft@gmail.com>
2021-11-24 22:55:56 -0300

    Video #12 - Part 1 (small rollback)

--- bsc-payments-processor/bsc-payments-processor-sv/src/app/services/index.ts
+++ bsc-payments-processor/bsc-payments-processor-sv/src/app/services/index.ts
@@ -2,4 +2,3 @@ export { BscScanApi } from './bsc-scan-api.service';
 export { Transactions } from './transactions.service';
 export { Wallets } from './wallets.service';
 export { ReceivingWallets } from './receiving-wallets.service';
-export { PaymentsProcessor } from './payments-processor.service';
