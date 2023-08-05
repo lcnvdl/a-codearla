b5e6de29f0799f5f08de10ec4e877c0ed5c4a16a
Lcnvdl <programando.soft@gmail.com>
2023-08-05 14:29:41 -0300

    Realtime chart working.

--- apexcharts-ionic-angular-realtime/src/app/components/chart-ejemplo/chart-ejemplo.component.ts
+++ apexcharts-ionic-angular-realtime/src/app/components/chart-ejemplo/chart-ejemplo.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 // eslint-disable-next-line max-len
-import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexStroke, ApexDataLabels, ApexYAxis, ApexTitleSubtitle, ApexLegend } from 'ng-apexcharts';
+import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexStroke, ApexDataLabels, ApexYAxis, ApexTitleSubtitle, ApexLegend, ChartComponent } from 'ng-apexcharts';
 import { series } from './datos-ejemplo';
 
 export type ChartOptions = {
@@ -22,7 +22,10 @@ export type ChartOptions = {
   styleUrls: ['./chart-ejemplo.component.scss'],
 })
 export class ChartEjemploComponent implements OnInit {
+  @ViewChild('chartEnTiempoReal') chart: ChartComponent;
+
   chartOptions: ChartOptions;
+
 
   constructor() {
   }
@@ -71,5 +74,28 @@ export class ChartEjemploComponent implements OnInit {
 
     //  Llama al evento "resize", actualizando el chart.
     setTimeout(() => (window as any).dispatchEvent(new Event('resize')), 1);
+
+    //  Obtener nuevos datos
+    const newPrices = [...series.monthDataSeries1.prices];
+    const newDates = [...series.monthDataSeries1.dates];
+    let newDate = new Date(2017, 11, 8);
+
+    setInterval(() => {
+      const nuevoDato = newPrices[newPrices.length - 1] * (0.5 + Math.random());
+      newPrices.shift();
+      newPrices.push(nuevoDato);
+
+      newDate = new Date(newDate.getTime() + 1000 * 60 * 60 * 24);
+      newDates.shift();
+      newDates.push(newDate.toISOString());
+
+      this.chart.updateOptions({
+        labels: [...newDates]
+      });
+
+      this.chart.updateSeries([{
+        data: [...newPrices]
+      }], true);
+    }, 1000);
   }
 }
