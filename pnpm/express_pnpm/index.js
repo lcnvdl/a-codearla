bac9a59d02eca2579a9849b788fe008f200ca2ba
Lucho <programando.soft@gmail.com>
2023-12-06 07:22:57 -0300

    pnpm

--- pnpm/express_pnpm/index.js
+++ pnpm/express_pnpm/index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+import express from 'express';
+import { exec } from 'child_process';
+import { mkConfig, generateCsv, asString } from 'export-to-csv';
+
 const app = express();
 const port = 3000;
 
@@ -6,10 +9,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/csv', (req, res) => {
+  const csvConfig = mkConfig({ useKeysAsHeaders: true });
+
+  const mockData = [{
+    name: "Rouky",
+    date: "2023-09-01",
+    percentage: 0.4,
+    quoted: '"Pickles"',
+  },
+  {
+    name: "Keiko",
+    date: "2023-09-01",
+    percentage: 0.9,
+    quoted: '"Cactus"',
+  }];
+
+  const csv = generateCsv(csvConfig)(mockData);
+  res.send(asString(csv));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
+const start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
 const url = `http://localhost:${port}`;
-require('child_process').exec(start + ' ' + url);
+exec(start + ' ' + url);
---
+++ pnpm/readme.md
@@ -0,0 +1,24 @@
+# a-codearla
+
+## PNPM
+[PNPM](https://pnpm.io/es/) es una alternativa al clásico NPM (gestor de paquetes de javascript).
+
+La P viene de "Performance". El objetivo del mismo es solucionar algunos problemas que trae NPM.
+
+El principal problema de NPM es que, en cada proyecto, este genera la carpeta `node_modules` con miles y miles de archivos.
+
+Si tenemos 100 proyectos que utilizan la misma librería, en la misma versión, esto llenará el disco con miles (y por qué no millones) de archivos, y ocupará mucho espacio.
+
+`PNPM` soluciona esto utilizando `Enlaces simbólicos`.
+
+Les dejo un enlace al video en el que explico en detalle cómo funciona.
+
+FILLME
+
+## Links
+* Sitio oficial de pnpm: [https://pnpm.io/es/](https://pnpm.io/es/).
+
+## Sígueme
+[Twitter - @acodearla](https://twitter.com/acodearla)
+
+[Canal de YouTube - @ACodearla](https://www.youtube.com/c/ACodearla)
